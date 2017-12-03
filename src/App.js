5931085131d660d50b3697ce823abea575e0525c
negrolio <elnegroleo9@gmail.com>
2017-12-03 16:51:51 -0300

    trying css grid

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import QuestionBuilder from './components/QuestionBuilder/index.js';
 import project from './fakeDataBase/project1';
 import Table from './components/Table';
+import Button from './components/Button/index';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      showConstructor: false,
+      showPlay: false
+    }
+
+    this.showConstructor = this.showConstructor.bind(this);
+    this.showPlay = this.showPlay.bind(this);
+  }
+
+  showConstructor () {
+    this.setState((prevState)=>({
+      showConstructor: !prevState.showConstructor
+    }));
+  }
+  showPlay () {
+    this.setState((prevState)=>({
+      showPlay: !prevState.showPlay
+    }))
+  }
   render() {
+    const { showConstructor, showPlay } = this.state;
 
     return (
       <div className="App">
-        {/* <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>{project.name}</h2>
-        </div> */}
-        {<Table />   }
-        {/* <QuestionBuilder projects={project} /> */}
+        <div className="wrapper">
+          <div className="header">
+            <h1>{ project.name }</h1>
+          </div>
+          <div className="sidebar">
+            {!showConstructor && <Button label={'New Project'} eventOnClick={this.showConstructor} />}
+            {!showPlay && <Button label={'Play'} eventOnClick={this.showPlay} />}
+          </div>
+          <div className="content">
+            {showConstructor && <Table />   }
+            {showPlay && <QuestionBuilder projects={project} />}
+          </div>
+          <div className="footer"></div>
+        </div>
+
+
       </div>
     );
   }
